9a59aaba5baa1ea7478b4eafca99a35b8296225f
Aleksey Kurylev <chepe4pi@gmail.com>
2015-11-18 17:21:48 +0300

    add core app and map app

--- sokoban/settings.py
+++ sokoban/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'map',
+    'sk_core',
+    'sk_map',
 )
 
 MIDDLEWARE_CLASSES = (
