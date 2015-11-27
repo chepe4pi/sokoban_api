8e74025d00c93b1622068c3c60617d4531cc3c94
Aleksey Kurylev <chepe4pi@gmail.com>
2015-11-27 14:26:45 +0300

    add core filters

--- sk_core/filters.py
+++ sk_core/filters.py
@@ -1,11 +1,20 @@
 from rest_framework.filters import BaseFilterBackend
 
 
+# TODO Remove class if unused
 class OwnerFilterBackend(BaseFilterBackend):
     """
     Base filter - allows users to see only own objects.
     """
 
     def filter_queryset(self, request, queryset, view):
-        if request.user.is_authenticated():
-            return queryset.filter(owner=request.user)
+        return queryset.filter(owner=request.user)
+
+
+class AggrigatorOwnerFilterBackend(BaseFilterBackend):
+    """
+    Base filter - allows users to see only own objects.
+    """
+
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(aggrigator__owner=request.user)
