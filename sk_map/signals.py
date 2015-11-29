f8af8c5fa5014a95b22b97cf78dda3edffb87cc5
Aleksey Kurylev <chepe4pi@gmail.com>
2015-11-30 02:29:52 +0300

    create abstract signal handler and connector for Map save signal

--- sk_map/signals.py
+++ sk_map/signals.py
@@ -1,18 +1,13 @@
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 from .models import Map, Wall, Box, Point, Men
+from sk_core.signals import set_child_models_attrs
+from django.db.models.signals import post_save
+from functools import partial
+
 
 models = [Wall, Box, Point, Men]
 attrs = ['owner', 'public']
+sender = Map
 
-
-@receiver(post_save, sender=Map, dispatch_uid='set_attributes_to_aggrigable_models')
-def handler(sender, **kwargs):
-    print('got')
-    for attr in attrs:
-        value = getattr(kwargs['instance'], attr)
-        for model in models:
-            instances = model.objects.filter(map=kwargs['instance'])
-            for instance in instances:
-                setattr(instance, attr, value)
-                instance.save()
+post_save.connect(receiver=partial(set_child_models_attrs, models=models, attrs=attrs),\
+                          sender=Map, dispatch_uid='set_child_models_attrs', weak=False)
