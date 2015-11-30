d42606401c87a301b8b619f201c58ec4770abc5a
Aleksey Kurylev <chepe4pi@gmail.com>
2015-12-01 02:24:29 +0300

    create  register test

--- sk_auth/serializers/users.py
+++ sk_auth/serializers/users.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import authenticate
 
 
 class BaseAuthSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(allow_null=True) # TODO choice one of options
+    email = serializers.EmailField(allow_null=True)
 
     class Meta:
         model = User
@@ -17,3 +19,27 @@ class RegisterSerializer(BaseAuthSerializer):
         user = User.objects.create_user(username=validated_data['username'], email=validated_data['email'],\
                                         password=validated_data['password'])
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    default_error_messages = {
+        'inactive_account': _('User account is disabled.'),
+        'invalid_credentials': _('Unable to login with provided credentials.'),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(LoginSerializer, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self, attrs):
+        self.user = authenticate(username=attrs.get('username'), password=attrs.get('password'))
+        if self.user:
+            if not self.user.is_active:
+                raise serializers.ValidationError(self.error_messages['inactive_account'])
+            return attrs
+        else:
+            raise serializers.ValidationError(self.error_messages['invalid_credentials'])
+
--- sokoban/urls.py
+++ sokoban/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from sk_map.api.map import MapsViewSet, MapDetailViewSet, WallViewSet, BoxViewSet, PointViewSet, MenViewSet
-from sk_auth.api.auth import RegisterView
+from sk_auth.api.auth import RegisterView, LoginAPIView
 
 
 router = DefaultRouter()
@@ -12,7 +12,7 @@ router.register(r'wall', WallViewSet)
 router.register(r'box', BoxViewSet)
 router.register(r'point', PointViewSet)
 router.register(r'men', MenViewSet)
-router.register(r'register', RegisterView)
+router.register(r'auth/register', RegisterView)
 urlpatterns = router.urls
 
 
@@ -20,10 +20,14 @@ urlpatterns_admin = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-
 urlpatterns_rest = patterns('',
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
                             )
 
+urlpatterns_auth = patterns('',
+    url(r'^auth/login/', LoginAPIView.as_view(), name='login_view')
+                            )
+
 urlpatterns += urlpatterns_admin
 urlpatterns += urlpatterns_rest
+urlpatterns += urlpatterns_auth
