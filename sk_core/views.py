bec58b4ea2e015a940919507db0ea8ac2c4b2070
Aleksey Kurylev <chepe4pi@gmail.com>
2015-12-07 20:00:26 +0300

    allow partial updates

--- sk_core/views.py
+++ sk_core/views.py
@@ -4,3 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 class BaseModelViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class PartialUpdateMixin(ModelViewSet):
+    def update(self, request, *args, **kwargs):
+        return super(PartialUpdateMixin, self).update(request, partial=True)
