52733f273c59eaf3044286e7be03c1a26a2d0470
chepe4pi <chepe4pi@gmail.com>
2016-07-10 00:17:25 +0300

    update settings local

--- sokoban/settings/local.py
+++ sokoban/settings/local.py
@@ -2,3 +2,14 @@ from .base import *
 
 
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sokoban',
+        'USER': 'sokoban',
+        'PASSWORD': 'sokoban',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+}
