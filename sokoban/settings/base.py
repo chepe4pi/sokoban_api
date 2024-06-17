2aff3979ff0b4d68596afc1c9c5baa8f03371fb9
Alexey Kurylev <chepe4pi@gmail.com>
2024-06-16 22:52:03 -0300

    add migr, change auth

--- sokoban/settings/base.py
+++ sokoban/settings/base.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
